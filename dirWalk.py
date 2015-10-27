52044dfdde80a886a6ce2367c043e641be4da445
Mike R <mikeup28@gmail.com>
2015-10-27 12:05:07 -0700

    Update to allow to get the file stats

--- dirWalk.py
+++ dirWalk.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 #Author: Mike R
 #walking the current working directory
-import os 
+import os
 i=0
 dash='---'
 #ask user for directory or take it.
 
 def dirWalk(thisDir, dash, i):
-	i=i+1
-	for item in os.listdir(thisDir):
-		fullPath=os.path.join(thisDir,item)
-		
-		if os.path.isfile(fullPath):
-			print (dash*i) + item
-		elif os.path.isdir(fullPath):
-			print (dash*i) + item
-			newDir=thisDir + "/" + item
-			dirWalk(newDir, dash, i)
-		else:
-			print "unknown!"		
+    i=i+1
+    for item in os.listdir(thisDir):
+        fullPath=os.path.join(thisDir,item)
+                
+        if os.path.isfile(fullPath):
+            print (dash*i) + item
+            print os.stat(fullPath) #get file information
+        elif os.path.isdir(fullPath):
+            print (dash*i) + item
+            newDir=thisDir + "/" + item
+            dirWalk(newDir, dash, i)
+        else:
+            print "unknown!"
 #see if there is a way to print the directory name, not necessarily full path
 directory=raw_input("What directory do you want to walk: ")
 #if one wanted to get all files, then one would need to be sudo
